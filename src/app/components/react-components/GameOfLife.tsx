323315e0ae0d6c90f5d375e653b65f5ad770c5e5
ArnavMeh <mehraarn000@gmail.com>
2022-01-07 16:03:36 -0500

    minor fixes
    mobile style fixes
    bg comp responsive fix

--- src/app/components/react-components/GameOfLife.tsx
+++ src/app/components/react-components/GameOfLife.tsx
@@ -1,100 +1,101 @@
 import React, {useState, useEffect} from 'react'
 
-const gridPad = 3;
 const animSpd = .4;
 const dRad = 50;
-const dbcolor = "rgb(15, 15, 15)";
+const dbcolor = "rgb(30, 30, 30)";
 const freq = 1;
 const bigDot = 10;
 const smallDot = 5;
-const bordered = false;
-const minDotCount = 100;
-const newShapePeriod = 1;
 const cellSize = 20;
 
 
-const GameOfLife = () => {
-    const [rows, setRows] = useState(Math.floor(window.innerHeight/cellSize));
-    const [cols, setCols] = useState(Math.floor(window.innerWidth/cellSize));
-    const [timeLock, setTimeLock] = useState(false);
-   
-
-    const rand = (n: number) => {
-        return Math.floor(Math.random() * n);
+const rand = (n: number) => {
+    return Math.floor(Math.random() * n);
+}
+const isValid = (r: number, c: number, 
+        rdim: number, cdim: number) => {
+    return (0 <= r && r < rdim
+            && 0 <= c && c < cdim);
+}
+const getShape = (str: string) => {
+    switch (str) {
+        case 'block': return [[0, 0], [0, 1], [1, 0], [1, 1]];
+        case 'blinker': return [[0, 0], [1, 0], [2, 0]];
+        case 'glider': return [[1, 0], [2, 1], [0, 2], [1, 2], [2, 2]];
+        case 'toad': return [[0, 1], [0, 2], [0, 3], [1, 0], [1, 1], [1, 2]];
+        default: return [];
     }
-    const getShape = (str: string) => {
-        switch (str) {
-            case 'block': return [0, 1, cols, cols+1]
-            case 'blinker': return [0, cols, cols*2]
-            case 'glider': return [cols, cols*2+1, 2, cols+2, cols*2+2]
-            case 'toad': return [1, 2, 3, cols, cols+1, cols+2]
-            default: return []
+}
+const addShape = (dotArr: boolean[][]) => {
+    var rows = dotArr.length;
+    var cols = dotArr[0].length;
+
+    var shape = ['glider', 'blinker', 'toad'][rand(3)];
+    const indexArr = getShape(shape);
+    var rowDisp = rand(rows);
+    var colDisp = rand(cols);
+
+    for (var i = 0; i < indexArr.length; i++) {
+        var r = indexArr[i][0] + rowDisp;
+        var c = indexArr[i][1] + colDisp;
+        if (isValid(r, c, rows, cols)) {
+            dotArr[r][c] = true;
         }
     }
+    return dotArr;
+}
+
+const resizeDots = (currRows: number, currCols: number, 
+        currDots: boolean[][]) => {
+    var dotArr = Array(currRows).fill(null).map(
+        () => Array(currCols).fill(false));
+    var rows = Math.min(currDots.length, currRows);
+    var cols = Math.min(currDots[0].length, currCols);
+    for (var r = 0; r < rows; r++) {
+        for (var c = 0; c < cols; c++) {
+            dotArr[r][c] = currDots[r][c];
+        }
+    }
+    return dotArr;
+}
+
+
+const GameOfLife = () => {
+
+    const getRows = () => {
+        return Math.floor(window.innerHeight/cellSize);
+    }
+    const getCols = () => {
+        return Math.floor(window.innerWidth/cellSize);
+    }
     const initMultiShapes = (num: number) => {
+        var rows = getRows();
+        var cols = getCols();
         var dotArr = Array(rows).fill(null).map(
             () => Array(cols).fill(false));
         for (var n = 0; n < num; n++) {
-            var r = rand((rows - 2) * (cols - 2));
-            var s = ['glider', 'blinker', 'toad'][rand(3)];
-            const indexArr = getShape(s);
-
-            for (var i = 0; i < indexArr.length; i++) {
-                var ind = indexArr[i] + r;
-                dotArr[Math.floor(ind / cols)][ind % cols] = true;
-            }
+            dotArr = [...addShape(dotArr)];
         }
         return dotArr;
     }
-    const rescaleDots = (rowNum: number, colNum: number) => {
-        var dotArr = Array(rowNum).fill(null).map(
-            () => Array(colNum).fill(false));
-        var comRows = Math.min(rowNum, dots.length)
-        var comCols = Math.min(colNum, dots[0].length)
-        for (var r = 0; r < comRows; r++) {
-            for (var c = 0; c < comCols; c++) {
-                dotArr[r][c] = dots[r][c];
-            }
-        }
-        setDots(dotArr);
-    }
 
     const [dots, setDots] = useState(initMultiShapes(40));
+    const [rows, setRows] = useState(getRows());
+    const [cols, setCols] = useState(getCols());
     const [t, setT] = useState(0);
-
-    window.addEventListener('resize', () => {
-        setTimeLock(true);
-        setTimeout(() => {
-            var r = Math.floor(window.innerHeight/cellSize)
-            var c = Math.floor(window.innerWidth/cellSize);
-            setRows(r);
-            setCols(c);
-            rescaleDots(r, c);
-            setTimeLock(false);
-        }, 1100)
-    });
     
-    const addShape = (startInd: number, shape: string) => {
-        const dotArr = dots
-        const indexArr = getShape(shape)
-        for (var i=0; i<indexArr.length; i++) {
-            var ind = indexArr[i] + startInd
-            dotArr[Math.floor(ind / cols)][ind % cols] = true
-        }
-        setDots([...dotArr])
-    }
     const getNB = (r: number, c: number) => {
-        if (0 <= r && r < rows
-                && 0 <= c && c < cols) {
-            return dots[r][c]
+        if (0 <= r && r < dots.length
+                && 0 <= c && c < dots[0].length) {
+            return dots[r][c];
         } else {
-            return 0
+            return 0;
         }
     }
     const updateDots = () => {
-        const newDots = dots
-        for (var i = 0; i < rows; i++) {
-            for (var j = 0; j < cols; j++) {
+        const newDots = [...dots];
+        for (var i = 0; i < newDots.length; i++) {
+            for (var j = 0; j < newDots[0].length; j++) {
                 var neighbors = 
                     getNB(i, j+1) + getNB(i, j-1) +
                     getNB(i-1, j) + getNB(i+1, j) +
@@ -107,48 +108,40 @@ const GameOfLife = () => {
                 }
             }
         }
-        setDots(newDots)
+        setDots([...newDots]);
     }
-    const countDots = () => {
-        var count = 0
-        for (var r = 0; r < rows; r++) {
-            for (var c = 0; c < cols; c++) {
-                if (dots[r][c]) { count++ }
-            }
-        }
-        return count
-    }
-
+    
     useEffect(() => {
         setTimeout(() => {
-            if (!timeLock) {
-                if (
-                    (minDotCount? countDots() < minDotCount : false) || 
-                    (newShapePeriod? t % newShapePeriod == 0 : false)
-                ) {
-                    var r = rand((rows - 2)*(cols - 2))
-                    var s = ['glider', 'blinker', 'toad'][rand(3)]
-                    addShape(r, s)
-                }
-                updateDots()
+            var currRows = getRows();
+            var currCols = getCols();
+            if (rows != currRows || cols != currCols) {
+                setRows(currRows);
+                setCols(currCols);
+                setDots(resizeDots(currRows, currCols, dots));
+            } else {
+                setDots(addShape(dots));
+                updateDots();
             }
-            setT(t+1)
-        }, 1000/freq)
-    }, [t])
-
-    
+            setT(t + 1);
+        }, 1000/freq);
+    }, [t])   
 
     return (
         <div style={{position: "fixed"}}>
-            {dots.map((dotRow) => 
-                <div style={{
-                    display: "flex",
-                    justifyContent:"center",
-                    height: cellSize
-                }}>
-                    {dotRow.map((dot) => {
+            {dots.map((dotRow, rowNum) => 
+                <div 
+                    key={rowNum}
+                    style={{
+                        display: "flex",
+                        justifyContent:"center",
+                        height: cellSize
+                    }}
+                >
+                    {dotRow.map((dot, colNum) => {
                         return (
-                            <div 
+                            <div
+                                key={colNum} 
                                 style={{
                                     width: cellSize,
                                     height: cellSize,
